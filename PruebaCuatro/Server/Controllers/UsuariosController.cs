7b4d13831a83c14469d0893732e18cf76e4f6e22
Marcos dalmo <marcosdalmolin@hotmail.com>
2022-05-20 00:14:46 -0400

    Agregar archivos de proyecto.

---
+++ PruebaCuatro/Server/Controllers/UsuariosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaCuatro.Server.Data;
+using PruebaCuatro.Shared.DTOs;
+
+namespace PruebaCuatro.Server.Controllers
+{
+    [ApiController]
+    [Route("api/usuarios")]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public UsuariosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: UsuariosController
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioDTO>>> Index()
+        {
+            return await context.Users.Select(x => 
+            new UsuarioDTO { Email = x.Email, UserId = x.Id }).ToListAsync(); 
+        }
+
+      
+    }
+}
